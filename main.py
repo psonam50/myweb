e7dcf01ff3d016b522531a1f3fea6f1a972500b3
psonam50 <psonam50@gmail.com>
2021-07-15 10:14:06 +0530

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template, SQLAlchemy
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/actecal'
+db=SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    phone_num = db.Column(db.String(13), unique=True, nullable=False)
+    msg = db.Column(db.String(120), unique=True, nullable=False)
+    date = db.Column(db.String(120))
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route("/contact", methods=['POST', 'GET'])
+def contact():
+    if request.method=='POST':
+        name= request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone_num')
+        message = request.form.get('message')
+
+        entry=Contacts(name=name, email=email, phone_num=phone, msg=message, date=datetime.date)
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html')
 
 
 @app.route('/index')
@@ -21,11 +48,6 @@ def about():
 @app.route('/post')
 def post():
     return render_template('post.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 
 if __name__ == '__main__':
