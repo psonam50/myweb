a078a7e59dd5af6a22527e0b470efb35acd9f083
psonam50 <psonam50@gmail.com>
2021-07-20 10:28:01 +0530

    update

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from datetime import datetime
+import json
+from flask_mail import Mail
+
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+local_server = True
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/actecal'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']= "465"
+app.config['MAIL_USERNAME']=params["gmail-user"]
+app.config['MAIL_PASSOWRD']=params["gmail-password"]
+app.config['MAIL_USE_TLS']=False
+app.config['MAIL_USE_SSL']=True
+mail= Mail(app)
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db=SQLAlchemy(app)
 
 class Contacts(db.Model):
@@ -18,7 +36,7 @@ class Contacts(db.Model):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 
 @app.route("/contact", methods=['POST', 'GET'])
@@ -32,22 +50,26 @@ def contact():
         entry=Contacts(name=name, email=email, phone_num=phone, msg=message, date=datetime.date)
         db.session.add(entry)
         db.session.commit()
-    return render_template('contact.html')
-
+        mail.send_message("new message from"+name,
+                          sender="email",
+                          recipients=[params["gmail-user"]],
+                          body=message
+                          )
+    return render_template('contact.html', params=params)
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 
 @app.route('/post')
 def post():
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 
 if __name__ == '__main__':
