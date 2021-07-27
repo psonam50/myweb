a40377893af3463f8592f049b035cc2dc0e10f79
psonam50 <psonam50@gmail.com>
2021-07-27 09:42:46 +0530

    update

--- main.py
+++ main.py
@@ -70,12 +70,33 @@ def about():
 def post():
     return render_template('post.html', params=params)
 
-@app.route('/setcookie')
-def set():
-    resp = make_response(render_template('about.html'))
-    resp.set_cookie('cookie', 'flask')
-    return resp
 
+@app.route('/cookie/')
+def cookie():
+    if not request.cookies.get('foo'):
+        res = make_response("Setting a cookie")
+        res.set_cookie('foo', 'bar', max_age=60*60*24*365*2)
+    else:
+        res = make_response("Value of cookie foo is {}".format(request.cookies.get('foo')))
+    return res
+
+@app.route('/delete-cookie/')
+def delete_cookie():
+    res = make_response("Cookie Removed")
+    res.set_cookie('foo', 'bar', max_age=0)
+    return res
+
+
+@app.route('/article/', methods=['POST', 'GET'])
+def article():
+    if request.method == 'POST':
+        print(request.form)
+        res = make_response("")
+        res.set_cookie("font", request.form.get('font'), 60 * 60 * 24 * 15)
+        res.headers['location'] = url_for('article')
+        return res, 302
+
+    return render_template('article.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=2000)
